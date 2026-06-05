238dcb8189d863159eedbb29b43dc5bcce942e2f
gabi569 <ontiverohumanogabrielagustin@gmail.com>
2026-06-05 10:57:14 -0300

    Add files via upload

---
+++ Arreglos ej2.1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Invertir(string[] arreglo)
+    {
+        int inicio = 0;
+        int fin = arreglo.Length - 1;
+
+        while (inicio < fin)
+        {
+            string aux = arreglo[inicio];
+
+            arreglo[inicio] = arreglo[fin];
+
+            arreglo[fin] = aux;
+
+            inicio++;
+            fin--;
+        }
+    }
+
+    static void Main()
+    {
+        string[] nombres = { "Ana", "Luis", "Pedro", "Maria" };
+
+        Invertir(nombres);
+
+        foreach (string nombre in nombres)
+        {
+            Console.Write(nombre + " ");
+        }
+    }
+}
