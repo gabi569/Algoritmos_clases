5efd8cc50a2ca8813d97d89bab35491fe293e6b1
gabi569 <ontiverohumanogabrielagustin@gmail.com>
2026-06-08 11:40:39 -0300

    Add files via upload

---
+++ Arreglos eje 2.3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        char[] Letras = { 'a', 'b', 'c', 'd', 'a', 'b', 'c', 'a', 'b' };
+        {
+            bool[] Visitado = new bool[Letras.Length];
+
+            for (int i = 0; i < Letras.Length; i++)
+            {
+                if (Visitado[i])
+                    continue;
+
+                int contador = 1;
+
+                for (int j = i + 1; j < Letras.Length; j++)
+                {
+                    if (Letras[i] == Letras[j])
+                    {
+                        contador++;
+                        Visitado[j] = true;
+                    }
+                }
+
+                Console.WriteLine(Letras[i] + " aparece " + contador + " veces ");
+            }
+        }
+    }
+}
