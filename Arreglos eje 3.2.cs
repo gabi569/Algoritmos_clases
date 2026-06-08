dd283a46ed71429ada9c2a740ffba9f0e3ee4d96
gabi569 <ontiverohumanogabrielagustin@gmail.com>
2026-06-08 12:29:40 -0300

    Add files via upload

---
+++ Arreglos eje 3.2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static int EliminarDuplicado(int[] nums)
+    {
+        if (nums.Length == 0)
+            return 0;
+
+        int Escritura = 1;
+
+        for (int Lectura = 1; Lectura < nums.Length; Lectura++)
+        {
+            if (nums[Lectura] != nums[Lectura - 1])
+            {
+                nums[Escritura] = nums[Lectura];
+                Escritura++;
+            }
+        }
+
+        return Escritura;
+    }
+
+    static void Main()
+    {
+        int[] nums = { 1, 1, 2, 2, 3, 4, 4, 5 };
+
+        int NuevaLongitud = EliminarDuplicado(nums);
+
+        Console.WriteLine("Nueva Longitud: " + NuevaLongitud);
+        
+        for (int i = 0; i < NuevaLongitud; i++)
+        {
+            Console.Write(nums[i] + " ");
+        }
+    }
+}
