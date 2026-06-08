e6ef0fbd80ef05ca84976bf892a4fe44e0e71b1b
gabi569 <ontiverohumanogabrielagustin@gmail.com>
2026-06-08 12:16:15 -0300

    Add files via upload

---
+++ Arreglo eje 3.1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] Arreglo = { 1, 2, 3, 4, 5, 6, 7 };
+        int rotaciones = 2;
+
+        rotaciones = rotaciones % Arreglo.Length;
+
+        int[] Resultado = new int[Arreglo.Length];
+
+        for (int i = 0; i < Arreglo.Length; i++)
+        {
+            Resultado[(i + rotaciones) % Arreglo.Length] = Arreglo[i];
+        }
+
+        foreach(int num in Resultado)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
